7cd6bc1e8b26dcb4d4ed6ac8578d905cb59012f8
leaf0n <96026903+leaf0n@users.noreply.github.com>
2021-12-12 17:28:26 -0600

    Nightshift-Master

---
+++ assets/preload/stages/exampleStage.lua
@@ -0,0 +1,15 @@
+function onCreate()
+	
+	addLuaSprite('nortonbg','bgnm',0,0)
+	addLuaSprite('nortonbg',false)
+	close(true)
+end
+
+function onBeatHit( ... )
+end
+
+function onStepHit ( ... )
+end
+
+function onUpdate( ... )
+end
